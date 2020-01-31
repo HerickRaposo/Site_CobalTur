82d34a8f047e0cfc757dab4b9246e4064ddeea1e
Hérick Raposo <herickraposo97@gmail.com>
2020-01-31 10:58:07 -0300

    A configurar
    Necessita configuração e ajustes finais no servidor

--- contrate_process.php
+++ contrate_process.php
@@ -18,7 +18,7 @@
     $logo = 'img/logo2.png';
     $link = '#';
 
-	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
+	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Contratante</title></head><body>";
 	$body .= "<table style='width: 100%;'>";
 	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
 	$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
